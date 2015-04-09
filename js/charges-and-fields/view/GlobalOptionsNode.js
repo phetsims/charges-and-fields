abac9aff31d4f60d451c6138f9410dca84b13313
Chris Malley <cmalley@pixelzoom.com>
2015-04-09 11:16:23 -0600

    remove extra space from copyright comment on line 1 of .js files

--- js/charges-and-fields/view/GlobalOptionsNode.js
+++ js/charges-and-fields/view/GlobalOptionsNode.js
@@ -1,4 +1,4 @@
-//  Copyright 2002-2014, University of Colorado Boulder
+// Copyright 2002-2014, University of Colorado Boulder
 
 /**
  * Global options shown in the "Options" dialog from the PhET Menu
