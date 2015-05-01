9575ea2c586b3252b47b9a18c1efb1d63cb33b3e
bereaphysics <martin.veillette@gmail.com>
2015-05-01 14:33:59 -0600

    get rid of unused methods

--- js/charges-and-fields/view/ChargesAndFieldsGlobals.js
+++ js/charges-and-fields/view/ChargesAndFieldsGlobals.js
@@ -1,18 +1,15 @@
-// Copyright 2002-2014, University of Colorado Boulder
+// Copyright 2002-2015, University of Colorado Boulder
 
 /**
- * Global settings and quality information
+ * Global settings
  *
- * @author Jonathan Olson <jonathan.olson@colorado.edu>
+ * @author Martin Veillette (Berea College)
  */
 define( function( require ) {
   'use strict';
 
   // modules
-  var Color = require( 'SCENERY/util/Color' );
-  var Property = require( 'AXON/Property' );
   var PropertySet = require( 'AXON/PropertySet' );
-  var Util = require( 'SCENERY/util/Util' );
 
   var ChargesAndFieldsGlobals = new PropertySet( {
     isElectricFieldLinesSupported: !!phet.chipper.getQueryParameter( 'electricFieldLines' ) || false,
@@ -20,64 +17,6 @@ define( function( require ) {
   } );
 
   return _.extend( ChargesAndFieldsGlobals, {
-    disallowWebGL : phet.chipper.getQueryParameter( 'webgl' ) === 'false',
-
-    /*
-     * Applies color changes to the material's color field, and also does so immediately upon being called.
-     *
-     * @param {THREE.Material} material
-     * @param {Property.<Color>} colorProperty
-     * @returns A callback that will unlink
-     */
-    linkColor: function( material, colorProperty ) {
-      var colorListener = function( color ) {
-        material.color.setHex( color.toNumber() );
-      };
-      colorProperty.link( colorListener );
-      return function() {
-        colorProperty.unlink( colorListener );
-      };
-    },
-
-    /*
-     * Applies color changes to the material's color and ambient fields, and also does so immediately upon being called.
-     *
-     * @param {THREE.Material} material
-     * @param {Property.<Color>} colorProperty
-     * @returns {Function} A callback that will unlink
-     */
-    linkColorAndAmbient: function( material, colorProperty ) {
-      var colorListener = function( color ) {
-        material.color.setHex( color.toNumber() );
-        material.ambient.setHex( color.toNumber() );
-      };
-      colorProperty.link( colorListener );
-      return function() {
-        colorProperty.unlink( colorListener );
-      };
-    },
-
-    /**
-     * Returns a color property
-     * @param {string||Color} color
-     * @returns {Property.<Color>}
-     */
-    toColorProperty: function( color ) {
-      // for now, cast it into place
-      var colorProperty;
-      if ( typeof color === 'string' ) {
-        color = new Color( color );
-      }
-      if ( color instanceof Color ) {
-        colorProperty = new Property( color );
-      }
-      else if ( color instanceof Property ) {
-        colorProperty = color;
-      }
-      else {
-        throw new Error( 'bad color passed to ChargesAndFieldsGlobals.toColorProperty' );
-      }
-      return colorProperty;
-    }
+    disallowWebGL : phet.chipper.getQueryParameter( 'webgl' ) === 'false'
   } );
 } );
