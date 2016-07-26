5394cb620bb49c73e4d6550b76a4b88d2dec44c6
Andrew Adare <andrewadare@gmail.com>
2016-07-26 09:44:34 -0600

    Move custom types into root sim directories (#503)

---
+++ js/simulations/charges-and-fields/TChargesAndFieldsModel.js
@@ -0,0 +1,27 @@
+// Copyright 2016, University of Colorado Boulder
+
+/**
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ * @author Andrew Adare
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var assertInstanceOf = require( 'PHET_IO/assertions/assertInstanceOf' );
+  var phetioNamespace = require( 'PHET_IO/phetioNamespace' );
+  var phetioInherit = require( 'PHET_IO/phetioInherit' );
+  var TObject = require( 'PHET_IO/types/TObject' );
+
+  // Instrumented to help restore charged particles.
+  var TChargesAndFieldsModel = phetioInherit( TObject, 'TChargesAndFieldsModel', function( instance, phetioID ) {
+    assertInstanceOf( instance, phet.chargesAndFields.ChargesAndFieldsModel );
+    TObject.call( this, instance, phetioID );
+  }, {}, {} );
+
+  phetioNamespace.register( 'TChargesAndFieldsModel', TChargesAndFieldsModel );
+
+  return TChargesAndFieldsModel;
+} );
+
