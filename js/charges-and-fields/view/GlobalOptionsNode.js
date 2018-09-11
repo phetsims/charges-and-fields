67f1efedfb02cc31195b4b4c4bbab20a0e593371
jbphet <john.blanco@colorado.edu>
2018-09-11 12:30:18 -0600

    integrated common ProjectorNodeCheckbox, see https://github.com/phetsims/joist/issues/342

--- js/charges-and-fields/view/GlobalOptionsNode.js
+++ js/charges-and-fields/view/GlobalOptionsNode.js
@@ -11,14 +11,10 @@ define( function( require ) {
   // modules
   var chargesAndFields = require( 'CHARGES_AND_FIELDS/chargesAndFields' );
   var ChargesAndFieldsGlobals = require( 'CHARGES_AND_FIELDS/charges-and-fields/view/ChargesAndFieldsGlobals' );
-  var Checkbox = require( 'SUN/Checkbox' );
   var inherit = require( 'PHET_CORE/inherit' );
   var LayoutBox = require( 'SCENERY/nodes/LayoutBox' );
   var OptionsDialog = require( 'JOIST/OptionsDialog' );
-  var Text = require( 'SCENERY/nodes/Text' );
-
-  // strings
-  var optionsProjectorColorsString = require( 'string!CHARGES_AND_FIELDS/options.projectorColors' );
+  var ProjectorModeCheckbox = require( 'JOIST/ProjectorModeCheckbox' );
 
   /**
    * @param {Tandem} tandem
@@ -26,14 +22,8 @@ define( function( require ) {
    */
   function GlobalOptionsNode( tandem ) {
 
-    // Static tandems are needed here because it is called before the sim is launched
-    var checkboxText = new Text( optionsProjectorColorsString, {
-      font: OptionsDialog.DEFAULT_FONT,
-      tandem: tandem.createTandem( 'projectorCheckboxString' )
-    } );
-
-    var projectorCheckbox = new Checkbox( checkboxText, ChargesAndFieldsGlobals.projectorColorsProperty, {
-      tandem: tandem.createTandem( 'projectorCheckbox' )
+    var projectorCheckbox = new ProjectorModeCheckbox( {
+      projectorModeEnabledProperty: ChargesAndFieldsGlobals.projectorColorsProperty
     } );
 
     LayoutBox.call( this, _.extend( {
@@ -48,4 +38,3 @@ define( function( require ) {
 
   return inherit( LayoutBox, GlobalOptionsNode );
 } );
-
