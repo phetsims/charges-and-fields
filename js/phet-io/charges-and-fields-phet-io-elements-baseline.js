ddc9b9d5783400e83e1e1cee65273ccfa5283930
zepumph <michael.kauzmann@colorado.edu>
2019-05-29 11:31:52 -0800

    update baseline files, https://github.com/phetsims/phet-io/issues/1466

--- js/phet-io/charges-and-fields-phet-io-elements-baseline.js
+++ js/phet-io/charges-and-fields-phet-io-elements-baseline.js
@@ -3337,7 +3337,7 @@ window.phet.phetio.phetioElementsBaseline =
     "chargesAndFields.navigationBar.phetButton.pickableProperty": {
       "phetioDocumentation": "Set whether the phetButton will be pickable (and hence interactive), see the NodeIO documentation for more details",
       "phetioEventType": "model",
-      "phetioFeatured": false,
+      "phetioFeatured": true,
       "phetioHighFrequency": false,
       "phetioPlayback": false,
       "phetioReadOnly": false,
