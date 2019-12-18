056a6747690f4268e4590e99d17f06338459638e
samreid <reids@colorado.edu>
2019-12-18 12:08:21 -0700

    Update activeProperty.phetioDocumentation, see https://github.com/phetsims/gravity-and-orbits/issues/293

--- js/phet-io/charges-and-fields-phet-io-elements-baseline.js
+++ js/phet-io/charges-and-fields-phet-io-elements-baseline.js
@@ -2,7 +2,7 @@
 window.phet.phetio.phetioElementsBaseline = assert &&
   {
     "chargesAndFields.chargesAndFieldsScreen.activeProperty": {
-      "phetioDocumentation": "Indicates whether the screen is active.  For single-screen simulations, the screen is always active.",
+      "phetioDocumentation": "Indicates whether the screen is currently displayed in the simulation.  For single-screen simulations, there is only one screen and it is always active.",
       "phetioDynamicElement": false,
       "phetioDynamicElementArchetype": false,
       "phetioEventType": "MODEL",
