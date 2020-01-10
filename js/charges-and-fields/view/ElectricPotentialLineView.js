903ee7c062894452bf2a6c29981aac39e7e58bc6
phet-dev <phetadmin@gmail.com>
2020-01-10 03:11:48 -0700

    update copyright dates from daily grunt work

--- js/charges-and-fields/view/ElectricPotentialLineView.js
+++ js/charges-and-fields/view/ElectricPotentialLineView.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2019, University of Colorado Boulder
+// Copyright 2015-2020, University of Colorado Boulder
 
 /**
  * View object, with a variety of Nodes stored on it to be added by the parent container. Responsible for the drawing
