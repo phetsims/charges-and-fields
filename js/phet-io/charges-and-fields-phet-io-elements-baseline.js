0115b995827ff217d1f15b64ed311cb9e49fb32b
chrisklus <christopher.klusendorf@colorado.edu>
2020-02-18 14:32:01 -0500

    Update phetioDocumentation, see https://github.com/phetsims/joist/issues/591

--- js/phet-io/charges-and-fields-phet-io-elements-baseline.js
+++ js/phet-io/charges-and-fields-phet-io-elements-baseline.js
@@ -5423,7 +5423,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "PropertyIO<ScreenIO>"
     },
     "chargesAndFields.general.soundManager.enabledProperty": {
-      "phetioDocumentation": "If this sim contains sound, then this Property will toggle whether sound is enabled ordisabled.",
+      "phetioDocumentation": "If the sim contains sound, then this Property will toggle whether sound is enabled or disabled.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": false,
