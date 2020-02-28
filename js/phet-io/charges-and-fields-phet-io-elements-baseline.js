916fa80c743b4ab688973c5643ff60cda3b7af95
zepumph <michael.kauzmann@colorado.edu>
2020-02-28 13:35:40 -0900

    update documentation, https://github.com/phetsims/studio/issues/103

--- js/phet-io/charges-and-fields-phet-io-elements-baseline.js
+++ js/phet-io/charges-and-fields-phet-io-elements-baseline.js
@@ -5437,7 +5437,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
     "chargesAndFields.general.soundManager.enhancedSoundEnabledProperty": {
-      "phetioDocumentation": "If the sim contains sound, then this Property will toggle whether \"enhanced\" sound is enabled or disabled.",
+      "phetioDocumentation": "If the sim contains sound, then this Property will toggle whether \"enhanced\" sound is enabled or disabled. Note that not all simulations that support sound also support enhanced sound.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": false,
