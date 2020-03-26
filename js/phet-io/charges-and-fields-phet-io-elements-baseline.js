e0aa5460b42e1fbef64b45f703aae88cfaa59712
chrisklus <christopher.klusendorf@colorado.edu>
2020-03-26 11:41:51 -0400

    Update phet-io files for soundManager doc changes, see https://github.com/phetsims/tandem/issues/108

--- js/phet-io/charges-and-fields-phet-io-elements-baseline.js
+++ js/phet-io/charges-and-fields-phet-io-elements-baseline.js
@@ -5488,7 +5488,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
     "chargesAndFields.general.view.soundManager": {
-      "phetioDocumentation": "Controls the simulation's sound. Note that this only applies to sims that support sound.",
+      "phetioDocumentation": "Controls the simulation's sound. For sims that do not support sound, this element and its children can be ignored.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": false,
@@ -5501,7 +5501,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "ObjectIO"
     },
     "chargesAndFields.general.view.soundManager.enabledProperty": {
-      "phetioDocumentation": "If the sim contains sound, then this Property will toggle whether sound is enabled or disabled.",
+      "phetioDocumentation": "Determines whether sound is enabled.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": true,
@@ -5514,7 +5514,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
     "chargesAndFields.general.view.soundManager.enhancedSoundEnabledProperty": {
-      "phetioDocumentation": "If the sim contains sound, then this Property will toggle whether \"enhanced\" sound is enabled or disabled. Note that not all simulations that support sound also support enhanced sound.",
+      "phetioDocumentation": "Determines whether enhanced sound is enabled. Enhanced sound is additional sounds that can serve to improve the learning experience for individuals with visual disabilities. Note that not all simulations that support sound also support enhanced sound. Also note that the value is irrelevant when enabledProperty is false.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": true,
