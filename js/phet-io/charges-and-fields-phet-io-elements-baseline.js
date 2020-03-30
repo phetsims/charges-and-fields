bfaac1d914c6acf93ec1fb0ca4e07055d7b58e8b
chrisklus <christopher.klusendorf@colorado.edu>
2020-03-30 14:45:37 -0400

    Update phet-io files for https://github.com/phetsims/joist/issues/597

--- js/phet-io/charges-and-fields-phet-io-elements-baseline.js
+++ js/phet-io/charges-and-fields-phet-io-elements-baseline.js
@@ -469,6 +469,19 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
+    "chargesAndFields.chargesAndFieldsScreen.nameProperty": {
+      "phetioDocumentation": "",
+      "phetioDynamicElement": false,
+      "phetioEventType": "MODEL",
+      "phetioFeatured": false,
+      "phetioHighFrequency": false,
+      "phetioIsArchetype": false,
+      "phetioPlayback": false,
+      "phetioReadOnly": false,
+      "phetioState": true,
+      "phetioStudioControl": true,
+      "phetioTypeName": "PropertyIO<NullableIO<StringIO>>"
+    },
     "chargesAndFields.chargesAndFieldsScreen.view": {
       "phetioDocumentation": "",
       "phetioDynamicElement": false,
