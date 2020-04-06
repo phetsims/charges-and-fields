ee1718901dc1f8a013e0b2f20db2422d3687426b
chrisklus <christopher.klusendorf@colorado.edu>
2020-04-06 13:31:59 -0400

    Update phet-io files, see https://github.com/phetsims/joist/issues/597

--- js/phet-io/charges-and-fields-phet-io-elements-baseline.js
+++ js/phet-io/charges-and-fields-phet-io-elements-baseline.js
@@ -470,10 +470,10 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
     "chargesAndFields.chargesAndFieldsScreen.nameProperty": {
-      "phetioDocumentation": "",
+      "phetioDocumentation": "The name of the screen. Changing this value will update the screen name for the screen's corresponding button on the navigation bar and home screen, if they exist. May be null for single-screen simulations.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
-      "phetioFeatured": false,
+      "phetioFeatured": true,
       "phetioHighFrequency": false,
       "phetioIsArchetype": false,
       "phetioPlayback": false,
