fdf595485c4b61a5dc4ac5358081adab86dd5b28
chrisklus <christopher.klusendorf@colorado.edu>
2020-04-27 17:02:01 -0400

    Update phet-io files, see https://github.com/phetsims/joist/issues/628

--- js/phet-io/charges-and-fields-baseline.js
+++ js/phet-io/charges-and-fields-baseline.js
@@ -5558,7 +5558,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
-    "chargesAndFields.general.view.simNameProperty": {
+    "chargesAndFields.general.model.simNameProperty": {
       "phetioDocumentation": "The name of the sim. Changing this value will update the title text on the navigation bar and the title text on the home screen, if it exists.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
