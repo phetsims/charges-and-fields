a6b7ada2109444100b000ebf4d0ad6b8795612d0
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:40 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/chargesAndFieldsStrings.ts
@@ -0,0 +1,43 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import chargesAndFields from './chargesAndFields.js';
+
+type StringsType = {
+  'charges-and-fields': {
+    'title': string
+  },
+  'plusOneNanoC': string,
+  'minusOneNanoC': string,
+  'electricField': string,
+  'directionOnly': string,
+  'voltage': string,
+  'values': string,
+  'grid': string,
+  'sensors': string,
+  'eFieldUnit': string,
+  'centimeterUnit': string,
+  'angleUnit': string,
+  'snapToGrid': string,
+  'voltageUnit': string,
+  'oneMeter': string,
+  'equipotential': string,
+  'pattern': {
+    '0value': {
+      '1units': string
+    }
+  },
+  'options': {
+    'projectorColors': string
+  }
+};
+
+const chargesAndFieldsStrings = getStringModule( 'CHARGES_AND_FIELDS' ) as StringsType;
+
+chargesAndFields.register( 'chargesAndFieldsStrings', chargesAndFieldsStrings );
+
+export default chargesAndFieldsStrings;
