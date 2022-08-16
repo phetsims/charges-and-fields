f86fb28b49f6d8a45daf591a3c1c1570c6bf2733
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:36 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/chargesAndFieldsStrings.ts
+++ js/chargesAndFieldsStrings.ts
@@ -5,34 +5,53 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import chargesAndFields from './chargesAndFields.js';
 
 type StringsType = {
   'charges-and-fields': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'plusOneNanoC': string;
+  'plusOneNanoCProperty': TReadOnlyProperty<string>;
   'minusOneNanoC': string;
+  'minusOneNanoCProperty': TReadOnlyProperty<string>;
   'electricField': string;
+  'electricFieldProperty': TReadOnlyProperty<string>;
   'directionOnly': string;
+  'directionOnlyProperty': TReadOnlyProperty<string>;
   'voltage': string;
+  'voltageProperty': TReadOnlyProperty<string>;
   'values': string;
+  'valuesProperty': TReadOnlyProperty<string>;
   'grid': string;
+  'gridProperty': TReadOnlyProperty<string>;
   'sensors': string;
+  'sensorsProperty': TReadOnlyProperty<string>;
   'eFieldUnit': string;
+  'eFieldUnitProperty': TReadOnlyProperty<string>;
   'centimeterUnit': string;
+  'centimeterUnitProperty': TReadOnlyProperty<string>;
   'angleUnit': string;
+  'angleUnitProperty': TReadOnlyProperty<string>;
   'snapToGrid': string;
+  'snapToGridProperty': TReadOnlyProperty<string>;
   'voltageUnit': string;
+  'voltageUnitProperty': TReadOnlyProperty<string>;
   'oneMeter': string;
+  'oneMeterProperty': TReadOnlyProperty<string>;
   'equipotential': string;
+  'equipotentialProperty': TReadOnlyProperty<string>;
   'pattern': {
     '0value': {
       '1units': string;
+      '1unitsProperty': TReadOnlyProperty<string>;
     }
   };
   'options': {
     'projectorColors': string;
+    'projectorColorsProperty': TReadOnlyProperty<string>;
   }
 };
 
