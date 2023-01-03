292e8b4c1a78eb7459fad614b583cadaf2ae646f
phet-dev <phetadmin@gmail.com>
2023-01-03 03:48:34 -0700

    update copyright dates from daily grunt work

--- js/charges-and-fields/model/ChargedParticle.js
+++ js/charges-and-fields/model/ChargedParticle.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2021, University of Colorado Boulder
+// Copyright 2014-2023, University of Colorado Boulder
 
 /**
  * Type of a charged particle, which has charge (+1 or -1) and a position.
--- js/charges-and-fields/model/ModelElement.js
+++ js/charges-and-fields/model/ModelElement.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2022, University of Colorado Boulder
+// Copyright 2014-2023, University of Colorado Boulder
 
 /**
  * Type for a user controlled movable element. The element can return to its origin when animated.
