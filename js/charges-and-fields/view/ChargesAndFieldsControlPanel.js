3f0083fdb0ff0b4e94514b8b45e2863be0a50c4b
phet-dev <phetadmin@gmail.com>
2025-02-08 03:36:48 -0700

    update copyright dates from daily grunt work

--- js/charges-and-fields/view/ChargesAndFieldsControlPanel.js
+++ js/charges-and-fields/view/ChargesAndFieldsControlPanel.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2022, University of Colorado Boulder
+// Copyright 2014-2025, University of Colorado Boulder
 
 /**
  * Control panel with Check Boxes that control the electrical Properties of the simulation
--- js/charges-and-fields/view/ChargesAndSensorsPanel.js
+++ js/charges-and-fields/view/ChargesAndSensorsPanel.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2024, University of Colorado Boulder
+// Copyright 2015-2025, University of Colorado Boulder
 
 /**
  * Scenery Node that contains an enclosure for a positive, a negative electric charge and an electric sensor.
