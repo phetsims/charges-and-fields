151f4415305ad43f77d04ceb677dd920d4b06eda
Sam Reid <reids@colorado.edu>
2025-07-31 07:14:14 -0600

    Convert to TypeScript, see https://github.com/phetsims/charges-and-fields/issues/208

--- js/charges-and-fields/view/ChargedParticleRepresentationNode.ts
+++ js/charges-and-fields/view/ChargedParticleRepresentationNode.ts
@@ -8,7 +8,7 @@
 
 import Shape from '../../../../kite/js/Shape.js';
 import Circle from '../../../../scenery/js/nodes/Circle.js';
-import Node from '../../../../scenery/js/nodes/Node.js';
+import Node, { NodeOptions } from '../../../../scenery/js/nodes/Node.js';
 import Path from '../../../../scenery/js/nodes/Path.js';
 import RadialGradient from '../../../../scenery/js/util/RadialGradient.js';
 import chargesAndFields from '../../chargesAndFields.js';
@@ -22,10 +22,10 @@ class ChargedParticleRepresentationNode extends Node {
   /**
    * Constructor for the scenery node of the charge
    *
-   * @param {number} charge
-   * @param {Object} [options] - Passed to Node
+   * @param charge
+   * @param options - Passed to Node
    */
-  constructor( charge, options ) {
+  public constructor( charge: number, options?: NodeOptions ) {
 
     super( options );
 
@@ -51,7 +51,7 @@ class ChargedParticleRepresentationNode extends Node {
 
     // Create and add a plus or minus sign on the center of the circle based on the charge of the particle
     const ratio = 0.6; // relative size of the sign shape relative to the radius of the Circle
-    const pathOptions = { centerX: 0, centerY: 0, lineWidth: CIRCLE_RADIUS * 0.3, stroke: 'white', pickable: false };
+    const pathOptions = { centerX: 0, centerY: 0, lineWidth: CIRCLE_RADIUS * 0.3, stroke: 'white', pickable: false } as const;
     if ( charge === 1 ) {
       // plus Shape representing a positive charge
       const plusShape = new Shape().moveTo( -CIRCLE_RADIUS * ratio, 0 )
