24bbdcf236940c05a0c996d1cb96c1b63f86a937
Sam Reid <reids@colorado.edu>
2025-07-31 08:24:09 -0600

    Convert to TypeScript, see https://github.com/phetsims/charges-and-fields/issues/208

--- js/charges-and-fields/view/ElectricPotentialMobileWebGLNode.ts
+++ js/charges-and-fields/view/ElectricPotentialMobileWebGLNode.ts
@@ -7,12 +7,15 @@
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
 
-import ObservableArrayDef from '../../../../axon/js/ObservableArrayDef.js';
+import { ObservableArray } from '../../../../axon/js/createObservableArray.js';
+import Property from '../../../../axon/js/Property.js';
 import Matrix3 from '../../../../dot/js/Matrix3.js';
-import WebGLNode from '../../../../scenery/js/nodes/WebGLNode.js';
+import ModelViewTransform2 from '../../../../phetcommon/js/view/ModelViewTransform2.js';
+import WebGLNode, { WebGLNodePainter, WebGLNodePainterResult } from '../../../../scenery/js/nodes/WebGLNode.js';
 import ShaderProgram from '../../../../scenery/js/util/ShaderProgram.js';
 import chargesAndFields from '../../chargesAndFields.js';
 import ChargesAndFieldsColors from '../ChargesAndFieldsColors.js';
+import ChargedParticle from '../model/ChargedParticle.js';
 
 // higher values support more particles, but may compromise performance
 const MAX_PARTICLES_LIMIT = 32;
@@ -20,13 +23,16 @@ const scratchFloatArray = new Float32Array( 9 );
 
 class ElectricPotentialMobileWebGLNode extends WebGLNode {
 
+  public readonly chargedParticles: ObservableArray<ChargedParticle>;
+  public readonly modelViewTransform: ModelViewTransform2;
+  public readonly isVisibleProperty: Property<boolean>;
+
   /**
-   * @param {ObservableArray<ChargedParticle>} chargedParticles - all the chargedParticles in this array are active
-   * @param {ModelViewTransform2} modelViewTransform
-   * @param {Property.<boolean>} isVisibleProperty
+   * @param chargedParticles - all the chargedParticles in this array are active
+   * @param modelViewTransform
+   * @param isVisibleProperty
    */
-  constructor( chargedParticles, modelViewTransform, isVisibleProperty ) {
-    assert && assert( ObservableArrayDef.isObservableArray( chargedParticles ), 'invalid chargedParticles' );
+  public constructor( chargedParticles: ObservableArray<ChargedParticle>, modelViewTransform: ModelViewTransform2, isVisibleProperty: Property<boolean> ) {
 
     super( ElectricPotentialMobilePainter, {
       layerSplit: true, // ensure we're on our own layer
@@ -46,10 +52,10 @@ class ElectricPotentialMobileWebGLNode extends WebGLNode {
 
     // particle added
     // TODO: I've seen this pattern elsewhere in the code https://github.com/phetsims/charges-and-fields/issues/203
-    chargedParticles.addItemAddedListener( particle => particle.positionProperty.link( invalidateSelfListener ) );
+    chargedParticles.addItemAddedListener( ( particle: ChargedParticle ) => particle.positionProperty.link( invalidateSelfListener ) );
 
     // particle removed
-    chargedParticles.addItemRemovedListener( particle => {
+    chargedParticles.addItemRemovedListener( ( particle: ChargedParticle ) => {
       invalidateSelfListener();
       particle.positionProperty.unlink( invalidateSelfListener );
     } );
@@ -57,21 +63,17 @@ class ElectricPotentialMobileWebGLNode extends WebGLNode {
 
   /**
    * Detection for how many particles we can support.
-   * @public
    */
-  static getNumberOfParticlesSupported() {
+  public static getNumberOfParticlesSupported(): number {
     const canvas = document.createElement( 'canvas' );
-    const gl = canvas.getContext( 'webgl' ) || canvas.getContext( 'experimental-webgl' );
-    return ElectricPotentialMobileWebGLNode.particlesSupportedForContext( gl );
+    const gl = canvas.getContext( 'webgl' );
+    return ElectricPotentialMobileWebGLNode.particlesSupportedForContext( gl! );
   }
 
   /**
-   * @public
-   *
-   * @param {WebGL2RenderingContext} gl
-   * @returns {number}
+   * @param gl
    */
-  static particlesSupportedForContext( gl ) {
+  public static particlesSupportedForContext( gl: WebGLRenderingContext ): number {
     const otherVectorCount = 7; // colors, matrix and one extra to be safe
     const maxVertexUniforms = gl.getParameter( gl.MAX_VERTEX_UNIFORM_VECTORS );
     return Math.min( MAX_PARTICLES_LIMIT, maxVertexUniforms - otherVectorCount );
@@ -80,16 +82,21 @@ class ElectricPotentialMobileWebGLNode extends WebGLNode {
 
 chargesAndFields.register( 'ElectricPotentialMobileWebGLNode', ElectricPotentialMobileWebGLNode );
 
-class ElectricPotentialMobilePainter {
+class ElectricPotentialMobilePainter implements WebGLNodePainter {
+
+  private readonly gl: WebGLRenderingContext;
+  private readonly node: ElectricPotentialMobileWebGLNode;
+  private readonly maximumNumParticles: number;
+  private displayShaderProgram: ShaderProgram;
+  private readonly vertexBuffer: WebGLBuffer;
 
   /**
-   * @param {WebGLRenderingContext} gl
-   * @param {WaveWebGLNode} node
+   * @param gl
+   * @param node
    */
-  constructor( gl, node ) {
+  public constructor( gl: WebGLRenderingContext, node: ElectricPotentialMobileWebGLNode ) {
     this.gl = gl;
     this.node = node;
-
     this.maximumNumParticles = ElectricPotentialMobileWebGLNode.particlesSupportedForContext( gl );
 
     const particleIndices = _.range( this.maximumNumParticles );
@@ -113,14 +120,14 @@ class ElectricPotentialMobilePainter {
       'uniform mat3 uMatrixInverse;', // matrix to transform from normalized-device-coordinates to the model
       'const float kConstant = 9.0;',
       // 'uniform vec3 charge0;', etc.
-      _.map( particleIndices, n => `uniform vec3 charge${n};` ).join( '\n' ),
+      particleIndices.map( n => `uniform vec3 charge${n};` ).join( '\n' ),
       'void main() {',
       // homogeneous model-view transformation
       '  vec2 modelPosition = ( uMatrixInverse * vec3( vPosition, 1 ) ).xy;',
 
       // compute the total, not worrying about div by zero (will be covered up by charge icon)
       '  float voltage = 0.0;',
-      _.map( particleIndices, n => `  voltage += charge${n}.z * kConstant / length( modelPosition - charge${n}.xy );` ).join( '\n' ),
+      particleIndices.map( n => `  voltage += charge${n}.z * kConstant / length( modelPosition - charge${n}.xy );` ).join( '\n' ),
 
       // rules to color pulled from ChangesAndFieldsScreenView
       '  if ( voltage > 0.0 ) {',
@@ -135,11 +142,11 @@ class ElectricPotentialMobilePainter {
     ].join( '\n' ), {
       attributes: [ 'aPosition' ],
       uniforms: [ 'uMatrixInverse', 'uZeroColor', 'uPositiveColor', 'uNegativeColor' ].concat(
-        _.map( particleIndices, n => `charge${n}` ) )
+        particleIndices.map( n => `charge${n}` ) )
     } );
 
     // we only need one vertex buffer with the same contents for all three shaders!
-    this.vertexBuffer = gl.createBuffer();
+    this.vertexBuffer = gl.createBuffer()!;
     gl.bindBuffer( gl.ARRAY_BUFFER, this.vertexBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, new Float32Array( [
       -1, -1,
@@ -150,13 +157,11 @@ class ElectricPotentialMobilePainter {
   }
 
   /**
-   * @public
-   *
-   * @param {Matrix3} modelViewMatrix
-   * @param {Matrix3} projectionMatrix
-   * @returns {number} - WebGLNode.PAINTED_NOTHING or WebGLNode.PAINTED_SOMETHING.
+   * @param modelViewMatrix
+   * @param projectionMatrix
+   * @returns - WebGLNode.PAINTED_NOTHING or WebGLNode.PAINTED_SOMETHING.
    */
-  paint( modelViewMatrix, projectionMatrix ) {
+  public paint( modelViewMatrix: Matrix3, projectionMatrix: Matrix3 ): WebGLNodePainterResult {
     const gl = this.gl;
     const displayShaderProgram = this.displayShaderProgram;
 
@@ -209,14 +214,11 @@ class ElectricPotentialMobilePainter {
 
   /**
    * Releases references
-   * @public
    */
-  dispose() {
+  public dispose(): void {
     // clears all of our resources
     this.displayShaderProgram.dispose();
     this.gl.deleteBuffer( this.vertexBuffer );
-
-    this.displayShaderProgram = null;
   }
 }
 
