6c1ee9a21b927a74efdbab149d47c782cdd0150f
Jonathan Olson <jonathan.olson@colorado.edu>
2025-08-27 19:35:37 -0600

    Only one export (named) for TReadOnlyProperty, see https://github.com/scenerystack/scenerystack/issues/55

--- js/charges-and-fields/view/ChargesAndSensorsPanel.ts
+++ js/charges-and-fields/view/ChargesAndSensorsPanel.ts
@@ -8,7 +8,7 @@
 
 import BooleanProperty from '../../../../axon/js/BooleanProperty.js';
 import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
-import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
+import { TReadOnlyProperty } from '../../../../axon/js/TReadOnlyProperty.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
 import ModelViewTransform2 from '../../../../phetcommon/js/view/ModelViewTransform2.js';
 import HBox from '../../../../scenery/js/layout/nodes/HBox.js';
